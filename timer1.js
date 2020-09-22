3e25ba8f80956684f760bd58de9d3804c8f26e96
NKadish <noahkadish@hotmail.com>
2020-09-22 17:36:34 +0000

    Just formatted a bit

--- timer1.js
+++ timer1.js
@@ -6,11 +6,10 @@ slicedTimer.sort(function(x, y) {
 });
 // now the the numbers have been sorted, we can loop through the array and get it to do what it wants when it wants.
 for (const time of slicedTimer) {
-  if (isNaN(time) === false && time > 0) {
-    let timeToJSTime = time * 100;
-    console.log(timeToJSTime)
+  if (isNaN(time) === false && time > 0) { // checks to make sure that the argument provided is a positive number
+    let timeToJSTime = time * 100; // turns the seconds given to seconds in javascript
     setTimeout(() => {
-      process.stdout.write('\x07');
+      process.stdout.write('\x07'); // outputs the alarm sound
     }, timeToJSTime);
   }
 }
